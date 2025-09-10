2d1a02e7d313f536bf1c8de5bd6824739c73c495
aswathamaallu007-star <aswathamaallu786@gmail.com>
2025-09-10 09:20:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 //your JS code here. If required.
 // const square1 = document.getElementId("square1")
 // const square2 = document.getElementId("square2")
-// const square3 = document.getElementId("square3")
-const squares = document.querySelectorAll(".square") 
-for(let square of squares){
-	square.addEventListener("mouseover",(e)=>addbgcolor(e))
-	square.addEventListener("mouseout",(e)=>addfontcolor(e))	
-}
 
-function addbgcolor(e){
-	const selectboxid = e.target.id
-	for(let i = 0;i<squares.length;i++){
-		if(squares[i].id !== selectboxid){
-			squares[i].style.backgroundColor = "#6F4E37";
-		}
-		
-	}
-	
+const squares = document.querySelectorAll("square")
+for(let square of squares){
+square.addEventListener("mouseover",colorchange)
+square.addEventListener("mouseout",colorback)
 }
-function addfontcolor(e){
+function colorchange(e){
+	const selectedsquare = e.target.id
 	for(let square of squares){
-		square.style.backgroundColor = "#E6E6FA";
+		if(selectedsquare != square.id){
+			square.style.backgroundColor = "#6F4E37";
+		}
 	}
+function colorback(){
+	for(let square of squares){
+	square.style.background = "#E6E6FA";}
+	
 }
